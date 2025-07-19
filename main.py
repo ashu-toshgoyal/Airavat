988f826f38cfa21e2414f24b533f386317d0aaa1
ashu-toshgoyal <ashu.toshgoyal1405@gmail.com>
2025-07-19 22:01:34 +0530

    Airavat Beginning

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-import csv
-import os
+import pyttsx3, os, tkinter,spacy
 import speech_recognition as sr
-import pyttsx3
-import webbrowser
-import datetime
-import wikipedia
-import pywhatkit
-import pyjokes
-import pywhatsapp
-import pywhatsapp_api
-import pywhatsapp_api_client
-
-
-def speak(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
-def takeCommand():
-    r = sr.Recognizer()     
+from tkinter import * 
+import tkinter as tk
