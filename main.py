f8710262df954efb437b9acfe4ca05fc20544764
ashu-toshgoyal <ashu.toshgoyal1405@gmail.com>
2025-07-20 17:51:55 +0530

    Upd

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
 import pyttsx3, os, tkinter,spacy
 import speech_recognition as sr
-from tkinter import * 
-# import tkinter as tk
-
-
-win = Tk()
-win.title("Airavat")
-win.geometry("600x400")
-
-opt = StringVar(win)
-
-
-
-opt.set("Select Voice")
-Voices_GUI = ["voice1","voice2","voice3"]
-Voption = OptionMenu(win, opt , *Voices_GUI)
-Voption.pack()
-
-win.mainloop()
+from ttkbootstrap import *
+from tkinter import HORIZONTAL, StringVar
