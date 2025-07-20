de3706ee56f0744b9ceb3a4e7410b45f31c1f91e
ashu-toshgoyal <ashu.toshgoyal1405@gmail.com>
2025-07-20 13:04:48 +0530

    Module nd wund

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pyttsx3, os, tkinter,spacy
 import speech_recognition as sr
 from tkinter import * 
-import tkinter as tk
+# import tkinter as tk
+
+
+win = Tk()
+win.title("Airavat")
+win.geometry("600x400")
+
+opt = StringVar(win)
+
+
+
+opt.set("Select Voice")
+Voices_GUI = ["voice1","voice2","voice3"]
+Voption = OptionMenu(win, opt , *Voices_GUI)
+Voption.pack()
+
+win.mainloop()
